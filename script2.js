7706fb37576257324071d8971b9b0782c5c754b4
charles-hua95 <167638783+charles-hua95@users.noreply.github.com>
2024-04-20 14:45:12 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,6 @@
 function initMap() {
-    // Coordinates for Chinatown
-    const chinatown = {lat: 43.653023233458946, lng: -79.39743229321462};
-    
-    // Initialize the map
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: chinatown
-    });
-
+    var uluru = {lat: -25.344, lng: 131.036};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
